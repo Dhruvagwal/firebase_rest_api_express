bb3e498c282591db9e56d492c6cbeddc1754ea38
Dhruv <spark.founder@gmail.com>
2021-02-19 11:09:14 +0530

    v1

--- functions/index.js
+++ functions/index.js
@@ -43,13 +43,95 @@ app.post('/api/create', (req, res)=>{
 })
 
 
-//read
+//read a Specific based on product id
 //Get
+app.get('/api/read/:id', (req, res)=>{
+    (async ()=>{
+        try {
+            const documents = db.collection(CONST.DBNAME).doc(req.params.id);
+
+            let products = await documents.get();
+
+            let response  = products.data();
+
+            return res.status(200).send(response)
+        }catch(err){
+            console.log(err)
+            return res.status(500).send('ERROR!')
+        }
+    })();
+})
+
+//read all products
+//Get
+app.get('/api/read', (req, res)=>{
+    (async ()=>{
+        try {
+
+            let query = db.collection(CONST.DBNAME);
+
+            let response = [];
+
+            await query.get().then(querySnapshot => {
+                let docs = querySnapshot.docs; //the results of the query
+
+                for (let doc of docs){
+                    const selectedItem = {
+                        id: doc.id,
+                        name: doc.data().name,
+                        description: doc.data().description,
+                        price: doc.data().price,
+                    }
+                    response.push(selectedItem)
+                }
+                return response;
+            })
+
+            return res.status(200).send(response)
+        }catch(err){
+            console.log(err)
+            return res.status(500).send('ERROR!')
+        }
+    })();
+})
+
 
 //update
 //put
+app.put('/api/update/:id', (req, res)=>{
+    (async ()=>{
+        try {
+            const document = db.collection(CONST.DBNAME).doc(req.params.id)
+            await document.update({
+                name : req.body.name,
+                description: req.body.description,
+                price : req.body.price
+            })
+            return res.status(200).send('SUCESS!')
+        }catch(err){
+            console.log(err)
+            return res.status(500).send('ERROR!')
+        }
+    })();
+})
+
+
 
 //Delete
+app.delete('/api/delete/:id', (req, res)=>{
+    (async ()=>{
+        try {
+            const document = db.collection(CONST.DBNAME).doc(req.params.id)
+
+            await document.delete();
+            
+            return res.status(200).send('SUCESS!')
+        }catch(err){
+            console.log(err)
+            return res.status(500).send('ERROR!')
+        }
+    })();
+})
  
 
 //Export the api to firebase Cloud Function
