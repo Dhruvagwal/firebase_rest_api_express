5454cfe9ce47a77ac8bfc9754cfa981f6a210580
Dhruv <spark.founder@gmail.com>
2021-02-19 09:35:29 +0530

    v1

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+//Routes
+app.get('/', (req, res)=>{
+    return res.status(200).send('Hello World!');
+})
+
+//create 
+//post
+
+//read
+//Get
+
+//update
+//put
+
+//Delete
+ 
+
+//Export the api to firebase Cloud Function
+exports.app = functions.https.onRequest(app);
